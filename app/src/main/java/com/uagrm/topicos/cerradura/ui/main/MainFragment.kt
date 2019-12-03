452e8126d32dffd0028bce7af6e297f612a11553
Miguel Angel <m.escobar_27@outlook.com>
2019-12-03 09:40:30 -0400

    primer commit

---
+++ app/src/main/java/com/uagrm/topicos/cerradura/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.uagrm.topicos.cerradura.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.uagrm.topicos.cerradura.R
+
+class MainFragment : Fragment(), View.OnClickListener {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onClick(v: View?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
